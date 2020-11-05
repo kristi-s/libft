c2c49ccb38c47d247f8c1ca3cf660b6fdf7a7b04
Кристина Смирнова <kristinasmirnova@Kristi-Pro.local>
2020-11-05 11:46:58 +0300

    test free

---
+++ test_free.c
@@ -0,0 +1,27 @@
+	void  ft_free(char **ptr, int i)
+	{
+	  while(i)
+	  {
+	    free(ptr[i]);
+	    i--;
+	  }
+	  free(ptr);
+	}
+	int main() {
+	  char **arr_ptr;
+	  int i;
+	  
+	  i = 0;
+	  if (!(arr_ptr = malloc(6 * sizeof(char *))))
+	    return 0;
+	  arr_ptr[5] = 0;
+	  while (i < 5)
+	  {
+	    if (!(arr_ptr[i] = malloc(5 * sizeof(char))))
+	      return 0;
+	    i++;
+	  }
+	  ft_free(arr_ptr, 4);
+	  
+	  return 0;
+	}
